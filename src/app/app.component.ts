19964a5abb13c3ac5b9f9f72914535e973799d32
SalomaoFerreira <salomao1991@gmail.com>
2022-01-04 16:15:20 -0500

    3. Toggle the displaying of that paragraph with the button created in the first step

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  isButtonPressed;
+  
+  constructor() {
+    this.isButtonPressed = false;
+  }
+
+  onDisplayButton() {
+    this.isButtonPressed = true;
+  }
 }
